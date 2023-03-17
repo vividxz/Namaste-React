819417acbafb558f7dc73c947f0491a8248159e5
vividxz <aa5454@srmist.edu.in>
2023-03-18 00:23:02 +0530

    container

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
-const heading = React.createElement("h1", {} , "Namaste from React!");
+const heading1 = React.createElement(
+"h1",
+ {
+
+ } ,
+  "Heading 1");
+const heading2 = React.createElement(
+"h2",
+ {
+
+ } ,
+"Heading 2");
+const container = React.createElement(
+"div",
+ {
+
+ } ,
+ [heading1, heading2]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(container);
